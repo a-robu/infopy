f8a85eb31336a645035a025bbee64a9d464b86fd
Andrei Robu <andrei.rocks.1992@gmail.com>
2017-10-27 21:30:59 +0100

    working labeled matrix

--- labeled_matrix.py
+++ labeled_matrix.py
@@ -6,7 +6,12 @@ assertSequenceEqual = unittest.TestCase('__init__').assertSequenceEqual
 skip = pytest.mark.skip
 
 class M():
+    ''' A matrix whose indices can be any hashable objects. ''' 
     def __init__(self, m, row_symbols=None, column_symbols=None):
+        ''' 
+        m will be transformed into a matrix using 'pymatrix.matrix'.
+        row_symbols and column_symbols can be of any sequence type.
+        '''
         self._matrix = (m if isinstance(m, pymatrix.Matrix)
             else pymatrix.matrix(m))
         self.row_symbols = (row_symbols if row_symbols
@@ -21,26 +26,35 @@ class M():
             self.column_symbols == other.column_symbols
         ])
 
+    def _reorder_rows(self, new_ordering):
+        assert set(self.row_symbols) == set(new_ordering)
+        reordered = [None] * self._matrix.numrows
+        for old_i, symbol in enumerate(self.row_symbols):
+            new_i = new_ordering.index(symbol)
+            reordered[new_i] = list(self._matrix.row(old_i))
+        return M(reordered, new_ordering, self.column_symbols)
+
     def __mul__(self, other):
         ''' Implements matrix multiplication with labeled rows and cols. '''
         if isinstance(other, M):
-            to_match = self.row_symbols
-            current_columns = other.column_symbols
-            raise NotImplementedError
+            to_match = self.column_symbols
+            fixed_other = other._reorder_rows(to_match)
+            return M(
+                self._matrix * fixed_other._matrix, 
+                self.row_symbols, 
+                fixed_other.column_symbols
+            )
         if isinstance(other, numbers.Number):
             return M(self._matrix * other, self.row_symbols, self.column_symbols)
+
+    def __str__(self):
+        return "M('{}', {}, {})".format(
+            self._matrix, repr(self.row_symbols), repr(self.column_symbols)
+        )
     
     def __rmul__(self, other):
+        ''' This should only happen when other is not a M. So a number. '''
         return self * other
-    
-    #     r = {}
-    #     for row in matrix:
-    #         r[column_symbols[row]] = ble
-
-    # def __mult__(self, other):
-    #     fix_rows(matrix, row_permutation)
-    #     return M.from_matrix(A * B, A.column_symbols, B.row_symbols)
-    #     #[3,2, 1, 3]
 
 class TestM():
     def test_auto_matrix(self):
@@ -49,14 +63,29 @@ class TestM():
     def test_eq(self):
         assert M('0', 'A', 'X') == M('0', 'A', 'X'), 'should equal copy pasted'
         assert M('9 9 9') != M('0 1 2'), 'different matrix content'
-        assert M('0', 'A', 'X') != M('0', 'B', 'X'), 'different row symbols'
-        assert M('0', 'A', 'X') != M('0', 'A', 'Y'), 'different column symbols'
+        assert M('0', 'A', '_') != M('0', 'B', '_'), 'different row symbols'
+        assert M('0', '_', 'X') != M('0', '_', 'Y'), 'different column symbols'
 
     def test_mult_number(self):
         example = M('1 2')
         expected = M('2 4')
         assert example * 2 == expected, 'matrix on left'
         assert 2 * example == expected, 'matrix on right'
+
+    def test_str(self):
+        assert str(M('1 2', 'A', 'AB')) == "M('1 2', 'A', 'AB')"
+
+    def test_row_symbols(self):
+        assert M('0 \n 1', 'AB', '_').row_symbols == 'AB', 'explicit labeling'
+        assertSequenceEqual(M('0 1 \n 2 3').row_symbols, [0, 1]), 'implicit row indices'
+
+    def test_column_symbols(self):
+        assert M('0 1', '_', 'AB').column_symbols == 'AB', 'explicit labeling'
+        assertSequenceEqual(M('0 1 \n 2 3').column_symbols, [0, 1]), 'implicit column indices'
+
+    def test_reorder_rows(self):
+        actual = M('0 \n 2 \n 1', 'ACB', '_')._reorder_rows('ABC')
+        assert actual == M('0 \n 1 \n 2', 'ABC', '_')
 
     def test_mult_matrix(self):
         A = M('0 0\n0 0', 'AB', 'XY')
